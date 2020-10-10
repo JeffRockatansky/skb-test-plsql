1c92f248a2c0a7945e19cb8aca4462e7a5a14dff
JeffRockatansky <cilyrik.velfr@gmail.com>
2020-10-10 20:10:00 +0500

    1.3 - v2

--- code/SQL/1.3.sql
+++ code/SQL/1.3.sql
@@ -1,3 +1,11 @@
+--drop table SKB_TSILYURIK_EMP;
+--commit;
+--/
+create table SKB_TSILYURIK_EMP (
+	tabnum number PRIMARY key
+	, username varchar2(4000) not null UNIQUE
+);
+/
 CREATE OR REPLACE PACKAGE skb_tsilyurik_accs is
 	type numrow is record (num number);
 	type t_num is table of numrow;
@@ -10,6 +18,8 @@ CREATE OR REPLACE PACKAGE BODY skb_tsilyurik_accs is
 		my_tname CONSTANT tab.tname%type := 'SKB_TSILYURIK_EMP';
 		tname tab.tname%type := null;
 	begin
+		dbms_output.put_line(arg_n_count);
+		begin
 		select
 			t.tname
 			into tname
@@ -20,23 +30,21 @@ CREATE OR REPLACE PACKAGE BODY skb_tsilyurik_accs is
 					NO_DATA_FOUND
 				THEN
 					tname := NULL;
-		if tname = null then
-			execute immediate
-q'[create table ]'||my_tname||q'[ (
-	tabnum number not null
-	, username varchar2(4000) not null
-);
-commit;]';
+		end;
+		dbms_output.put_line('test');
+		if tname is not null then
+			dbms_output.put_line('ok');
+			for i in 1..arg_n_count loop
+				insert into SKB_TSILYURIK_EMP values (i, 'user'||i);
+				commit;
+			end loop;
 		end if;
-		for i in 1..arg_n_count loop
-			insert into SKB_TSILYURIK_EMP values (i, 'user'||i);
-		end loop;
 	end gen;
 	function get(arg_v_username in varchar2) return t_num pipelined is
 	begin
 		for acid in (
 			select
-				emp.tabnum
+				emp.tabnum acid
 				from
 					skb_tsilyurik_emp emp
 				where
@@ -47,4 +55,13 @@ commit;]';
 		return;
 	end get;
 end skb_tsilyurik_accs;
+/
+begin
+	skb_tsilyurik_accs.gen(arg_n_count => 5);
+	commit;
+end;
+/
+select * from SKB_TSILYURIK_EMP;
+/
+select * from table(skb_tsilyurik_accs.get('user2'));
 /
