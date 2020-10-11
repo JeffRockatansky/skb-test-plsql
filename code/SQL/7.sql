e73949bc2a844b42273a96203f962e7f171b34a5
JeffRockatansky <cilyrik.velfr@gmail.com>
2020-10-11 16:44:50 +0500

    7 - parsing XML

--- code/SQL/7.sql
+++ code/SQL/7.sql
@@ -1,8 +1,9 @@
 declare
-	msg varchar2(4000);
 	type skb_coll_row is record (s varchar2(100), k varchar2(100), b varchar2(100));
 	type skb_coll is table of skb_coll_row;
 	msg_coll skb_coll;
+	msg varchar2(4000);
+	msg_xml xmltype;
 begin
 	-- XML generation from query
 	select
@@ -19,7 +20,16 @@ begin
 		from dual
 	) skb;
 	dbms_output.put_line(msg);
+	-- Writing string into XML
+	msg_xml := xmltype(msg);
 	-- XML parsing into table collection
-	
+	select
+		extractvalue(Value(skb_xml), '/SKB/S') as s
+		, extractvalue(Value(skb_xml), '/SKB/K') as k
+		, extractvalue(Value(skb_xml), '/SKB/B') as b
+	bulk collect into
+		msg_coll
+	from table(XMLSequence(Extract(msg_xml, '/SKB'))) skb_xml;
+	dbms_output.put_line(msg_coll(1).s||' '||msg_coll(1).k||' '||msg_coll(1).b);
 end;
 /
